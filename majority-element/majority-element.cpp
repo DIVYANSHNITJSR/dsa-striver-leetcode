5b5b52cad176a01383ecd65c03779af632c0e996
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-06-07 19:08:33 +0530

    Time: 16 ms (86.70%) | Memory: 19.6 MB (62.57%) - LeetSync

--- majority-element/majority-element.cpp
+++ majority-element/majority-element.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums)
-     {
-          map <int,int> mpp;
-          for (int i=0;i<nums.size();i++)
-          {
-              mpp[nums[i]]++;
-          } 
-          for (auto it:mpp)
-          {
-              if(it.second>(nums.size())/2)
-                return it.first;
-          }
-          return 0;
+     {int el;
+     int cnt=0;
+     //apply algorithm moore's voting
+           for (int i = 0; i < nums.size(); i++) {
+        if (cnt == 0) {
+            cnt = 1;
+            el = nums[i];
+        }
+        else if (el == nums[i]) cnt++;
+        else cnt--;
+    }
+     //checking if the stored element
+    // is the majority element:
+    int cnt1 = 0;
+    for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == el) cnt1++;
+    }
+
+    if (cnt1 > (nums.size() / 2)) 
+    return el;
+    return 0;
     }
 
 };
