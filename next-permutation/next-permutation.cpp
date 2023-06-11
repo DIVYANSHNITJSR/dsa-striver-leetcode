ae25e1e0c675cf07eb4ca5a8d45003bdab6eefce
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-06-11 14:58:41 +0530

    Time: 4 ms (68.44%) | Memory: 12.2 MB (22.68%) - LeetSync

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -1,18 +1,33 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        int n = nums.size();
-        int i = n - 2;
-        while (i >= 0 && nums[i] >= nums[i + 1]) {
-            i--;
-        }
-        if (i >= 0) {
-            int j = n - 1;
-            while (j > i && nums[j] <= nums[i]) {
-                j--;
-            }
-            swap(nums[i], nums[j]);
-        }
-        reverse(nums.begin() + i + 1, nums.end());
+        next_permutation(nums.begin(),nums.end());
+        // int n = nums.size();
+        // int i = n - 2;
+        // while (i >= 0 && nums[i] >= nums[i + 1]) {
+        //     i--;
+        // }
+        // if (i==-1)
+        // {  reverse(nums.begin(),nums.end());
+        //   return;
+        // }int j;
+        // int result=0;
+        // for (int j=i+1;i<n;j++)
+        // {  
+        //     if(nums[j]>nums[i]  && nums[j]<result)
+        //         {
+        //              result=nums[j];
+
+        //         }
+        // }
+        // int k;
+        // for (int k=i+1;k<n;k++)
+        //    {
+        //        if(nums[k]==result)
+        //          break;
+        //    }
+        // swap(nums[i],nums[k] );
+        // sort(nums.begin() + (i + 1), nums.end());
     }
 };
+
