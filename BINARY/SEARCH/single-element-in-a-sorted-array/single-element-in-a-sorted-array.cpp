48c2ede2f1db5b9c910262ad7858ca8a51dcfd16
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-06-16 18:14:04 +0530

    Time: 30 ms (52.59%) | Memory: 22.3 MB (37.88%) - LeetSync

--- BINARY/SEARCH/single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ BINARY/SEARCH/single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -17,16 +17,15 @@ public:
         while (low<=high)
         {
             mid=(low+high)/2;
-            if(n==1)
-              return nums[mid];
+           
             if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1])
-               return nums[mid];
+               ans= nums[mid];
 
            if((mid%2==0 && nums[mid]==nums[mid+1])||(mid%2==1 &&nums[mid]==nums[mid-1] ))
                         low=mid+1;
              else
                         high=mid-1;
         }   
-        return -1;
+        return ans;
     }
 };
