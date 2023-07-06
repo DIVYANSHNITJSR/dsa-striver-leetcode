15f8ccc8c837a203f7d33c035352e4de3d814add
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-07-07 02:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 12.1 MB (57.48%) - LeetSync

--- BINARY/SEARCH/binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
+++ BINARY/SEARCH/binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.cpp
@@ -13,38 +13,40 @@ class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ans;
-        if (root == nullptr) {
-            return ans;
+        int level;
+        vector <int> v;
+        int h=hight(root);
+        for(level=0;level<h;level++)
+        {
+              vector <int> v;
+               traversal (root,level,v);
+                  if(level%2==0)
+                   ans.push_back(v);
+                  else
+                    {reverse(v.begin(),v.end());
+                     ans.push_back(v);
+                    }
         }
-
-        int count = 0;
-        queue<TreeNode*> q;
-        q.push(root);
-
-        while (!q.empty()) {
-            int size = q.size();
-            vector<int> level(size);
-
-            for (int i = 0; i < size; i++) {
-                TreeNode* current = q.front();
-                q.pop();
-
-                // Determine the index to place the node value based on the traversal direction
-                int index = (count % 2 == 0) ? i : size - 1 - i;
-                level[index] = current->val;
-
-                if (current->left != nullptr) {
-                    q.push(current->left);
-                }
-                if (current->right != nullptr) {
-                    q.push(current->right);
-                }
-            }
-
-            ans.push_back(level);
-            count++;
-        }
-
         return ans;
     }
+    public:
+        int hight(TreeNode* root)
+           {    if(root==NULL)
+                   return 0;
+               int l=hight(root->left);
+               int r=hight(root->right);
+               return max(l,r)+1;
+           }
+        void traversal (TreeNode* root,int level,vector <int>& v)
+        {
+            if(root==NULL)
+               return;
+            if(level==0)
+            v.push_back(root->val);
+            if(level>0)
+            {
+                traversal(root->left,level-1,v);
+                traversal(root->right,level-1,v);
+            }
+        }
 };
