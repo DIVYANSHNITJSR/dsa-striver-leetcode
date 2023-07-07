9d79048de069280773cc1165e1bca2204f71e069
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-07-07 18:08:18 +0530

    Time: 8 ms (33.50%) | Memory: 15.6 MB (5.66%) - LeetSync

--- BINARY/SEARCH/vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
+++ BINARY/SEARCH/vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.cpp
@@ -11,25 +11,51 @@
  */
 class Solution {
 public:
-    vector<vector<int>> verticalTraversal(TreeNode* root) {
-        map<int, map<int, multiset<int>>> nodes;
-        traverse(root, 0, 0, nodes);
+    vector<vector<int>> verticalTraversal(TreeNode* root) 
+    {   
+        // Create a vector to store the vertical traversal result
         vector<vector<int>> ans;
-        for (auto p : nodes) {
+        
+        // Create a map to store nodes based on their x and y coordinates
+        // Use multiset to automatically sort the values at each coordinate
+        map<int, map<int, multiset<int>>> mp;
+        
+        // Call the traverse function to populate the map
+        traverse(root, 0, 0, mp);
+        
+        // Iterate over the map to collect the values in the correct order
+        for (auto p : mp)
+        {
             vector<int> col;
-            for (auto q : p.second) {
+            
+            // Iterate over the multiset at each coordinate to get sorted values
+            for (auto q : p.second)
+            {
+                // Insert all the values from the multiset into the column vector
                 col.insert(col.end(), q.second.begin(), q.second.end());
             }
+            
+            // Add the column vector to the result
             ans.push_back(col);
         }
+        
+        // Return the vertical traversal result
         return ans;
     }
-private:
-    void traverse(TreeNode* root, int x, int y, map<int, map<int, multiset<int>>>& nodes) {
-        if (root) {
-            nodes[x][y].insert(root -> val);
-            traverse(root -> left, x - 1, y + 1, nodes);
-            traverse(root -> right, x + 1, y + 1, nodes);
-        }
-    }
+
+public:
+     // Recursive helper function to traverse the binary tree and populate the map
+     void traverse(TreeNode* root, int x, int y, map<int, map<int, multiset<int>>>& mp)
+     {
+         // Base case: if root is nullptr, return
+         if (root == nullptr)
+            return;
+        
+        // Insert the value of the current node into the map at the appropriate coordinates
+        mp[x][y].insert(root->val);
+        
+        // Recursively traverse the left and right subtrees, adjusting the coordinates accordingly
+        traverse(root->left, x - 1, y + 1, mp);
+        traverse(root->right, x + 1, y + 1, mp);
+     }
 };
