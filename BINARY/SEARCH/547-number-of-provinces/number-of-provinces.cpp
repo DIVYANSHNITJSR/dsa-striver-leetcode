59bebaaaf4021d3f2f61015daba7bd5a8b0c093a
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-07-29 19:19:31 +0530

    Time: 18 ms (98.01%) | Memory: 15.3 MB (39.84%) - LeetSync

---
+++ BINARY/SEARCH/547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int findCircleNum(vector<vector<int>>& con)
+     {   int count=0;
+        int v=con.size();
+        vector<int> vis(v,0);
+        for(int i=0;i<v;i++)
+          {
+              if(!vis[i])
+           {
+              count++;
+               dfs(con,vis,i);
+           }
+          }
+          return count;
+    }
+    public:
+        void dfs( vector<vector<int>>& con,vector<int>& vis,int i)
+        {
+            vis[i]=1;
+            for(int j=0;j<con.size();j++)
+               {
+                   if(con[i][j]==1 && !vis[j])
+                   {
+                       dfs(con,vis,j);
+                   }
+               }
+        }
+};
