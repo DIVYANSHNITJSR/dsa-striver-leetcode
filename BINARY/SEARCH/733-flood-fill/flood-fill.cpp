745d00460c2446a0b1b1ad570d634ad00d4c15b7
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-08-05 19:02:24 +0530

    Time: 8 ms (71.37%) | Memory: 13.9 MB (63.69%) - LeetSync

--- BINARY/SEARCH/733-flood-fill/flood-fill.cpp
+++ BINARY/SEARCH/733-flood-fill/flood-fill.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color)
-     {  int m=image.size();
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+         vector <vector<int>> ans=image;
+         int initialColor=image[sr][sc];
+         image[sr][sc]=color;
+         int drow[]={-1,0,+1,0};
+         int dcol[]={0,+1,0,-1};
+         dfs(image,ans,initialColor,drow,dcol,color,sr,sc);
+         return ans;
+    }
+    void dfs(vector<vector<int>>& image,vector<vector<int>>& ans,int initialColor,int drow[],int dcol[],int color,int row,int col)
+    {   int nrow,ncol;
+        ans[row][col]=color;
+        int m=image.size();
         int n=image[0].size();
-        int initialValue=image[sr][sc];
-        image[sr][sc]=color;
-        int drow[]={-1,0,+1,0};
-        int dcol[]={0,1,0,-1};
-        queue<pair<int,int>> q;
-        vector<vector<int>> vis(m,vector<int>(n,0));
-        q.push({sr,sc});
-        vis[sr][sc]=1;
-        while(!q.empty())
-        {int r=q.front().first;
-        int c=q.front().second;
-        q.pop();
-    for (int i = 0; i < 4; i++) 
-    {
-    int nrow = r + drow[i];
-    int ncol = c + dcol[i];
-    if (ncol >= 0 && ncol < n && nrow >= 0 && nrow < m && !vis[nrow][ncol] && image[nrow][ncol] == initialValue)
-    {
-        image[nrow][ncol] = color;
-        q.push({nrow, ncol});
-        vis[nrow][ncol]=1;
-    }
-    }
+        for(int i=0;i<4;i++)
+        {
+            nrow=row+drow[i];
+            ncol=col+dcol[i];
+            if(nrow>=0 && nrow<m && ncol>=0 && ncol<n && ans[nrow][ncol]!=color && image[nrow][ncol]==initialColor)
+            {
+                dfs(image,ans,initialColor,drow,dcol,color,nrow,ncol); 
+           }
 
         }
-        return image;
     }
 };
