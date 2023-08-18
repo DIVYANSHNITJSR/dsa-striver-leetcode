d6689650a84d73646db4a3cefcdb186c797a4ee6
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-08-18 19:54:13 +0530

    Time: 77 ms (70.86%) | Memory: 34.4 MB (42.69%) - LeetSync

--- BINARY/SEARCH/542-01-matrix/01-matrix.cpp
+++ BINARY/SEARCH/542-01-matrix/01-matrix.cpp
@@ -1,56 +1,45 @@
-#include <vector>
-#include <queue>
-#include <climits>
-
-using namespace std;
-
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size();
-        if (m == 0) return mat;
-        int n = mat[0].size();
-        
-        using Pair = pair<int, int>;
-        queue<Pair> q;
-        
-        vector<vector<int>> dis(m, vector<int>(n, INT_MAX)); 
-        
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
-                    dis[i][j] = 0;
-                    q.push({i, j});
-                }
+        int m=mat.size();
+        int n=mat[0].size();
+        queue <pair<pair<int,int>,int>> q;
+        vector<vector<int>> vis(m,vector<int>(n,0));
+        vector<vector<int>> dis(m,vector<int>(n,0));
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(mat[i][j]==0)
+                  {
+                       q.push({{i,j},0});
+                       vis[i][j]=1;
+                  }
+                  
             }
         }
-        
-        int drow[] = {-1, 0, 1, 0};
-        int dcol[] = {0, -1, 0, 1};
-        
-        while (!q.empty()) {   
-            int r = q.front().first;
-            int c = q.front().second;
+        int drow[]={0,-1,0,1};
+        int dcol[]={1,0,-1,0};
+        while(!q.empty())
+        {
+            int r=q.front().first.first;
+            int  c=q.front().first.second;
+            int minDis=q.front().second;
             q.pop();
-            
-            for (int i = 0; i < 4; i++) {
-                int newR = r + drow[i];
-                int newC = c + dcol[i];
-                
-                if (newR >= 0 && newR < m && newC >= 0 && newC < n) 
-                {
-                    if (dis[newR][newC] > dis[r][c] + 1) {
-                        dis[newR][newC] = dis[r][c] + 1;
-                        q.push({newR, newC});
-                    }
-                }
-            }
+            dis[r][c]=minDis;
+            for(int i=0;i<4;i++)
+              {
+                  int nrow=r+drow[i];
+                  int ncol=c+dcol[i];
+                  if(ncol>=0 && ncol<n && nrow>=0 && nrow<m && mat[nrow][ncol]==1 && !vis[nrow][ncol])
+                  {    
+                          vis[nrow][ncol]=1;
+                          q.push({{nrow,ncol},minDis+1});
+
+                      
+                  }
+              }
         }
-        
         return dis;
     }
 };
-
-
-
-
