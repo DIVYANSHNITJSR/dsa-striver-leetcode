ea939a5936bca71d37467c557da2b70a6f427179
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-09-02 15:03:37 +0530

    Time: 10 ms (99.40%) | Memory: 14 MB (41.93%) - LeetSync

---
+++ BINARY/SEARCH/207-course-schedule/course-schedule.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector <int> vis(numCourses,0);
+        vector<vector<int>> adj(numCourses);
+       for(int i = 0 ; i < prerequisites.size() ; i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        for(int i=0;i<numCourses;i++)
+           {
+               if(!vis[i])
+                 {
+                     if(detectCycle(i,adj,vis))
+                        return false;
+                 }
+           }
+        
+          return true;
+
+    }
+    bool detectCycle(int start,vector<vector<int>>& adj,vector<int>& vis )
+    {if(vis[start]==1) return true;
+     if(vis[start]==2)  return false;
+        vis[start]=1;
+        for(auto it:adj[start])
+           {
+            
+                  if(detectCycle(it,adj,vis))
+                      return true;
+                  
+                 
+           }
+           vis[start]=2;
+           return false;
+    }
+};
