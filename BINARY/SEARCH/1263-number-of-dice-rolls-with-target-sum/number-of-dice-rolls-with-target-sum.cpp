b37c7af9a120924ef46c0b9d841ad17f8caee4ca
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2023-12-26 22:37:18 +0530

    Time: 28 ms (42.81%) | Memory: 8.8 MB (65.29%) - LeetSync

--- BINARY/SEARCH/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ BINARY/SEARCH/1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int solve(int i,int k,int target,vector<vector<int>>& dp)
-    {    if(target<0) return 0;
-         if(i==0)  return target==0;
-          int modulo=1e9+7;
-       int ways=0;
-        if(dp[i][target]!=-1) return dp[i][target];
-        for(int face=1;face<=k;face++)
-        {
-            ways=(ways+solve(i-1,k,target-face,dp))%modulo;
-        }
-        return dp[i][target]=ways;
-    }
+   
     int numRollsToTarget(int n, int k, int target) {
-        vector<vector<int>> dp(n+1,vector<int>(target+1,-1));
-         
-       return solve(n,k,target,dp);
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+      dp[0][0]=1;
+        int mod=1e9+7;
+      
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=target;j++)
+                {   int ways=0;
+                     for(int face=1;face<=k;face++)
+                        {  if(j>=face)
+                            ways=(ways+dp[i-1][j-face])%mod;
+                        }
+                    dp[i][j]=ways;
+
+                }
+            }
+        
+        
+       return dp[n][target];
        
     }
 };
