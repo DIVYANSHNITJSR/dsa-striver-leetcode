e6962d15eba11ece3f0d8a43250836046cb78e11
DIVYANSHNITJSR <108193637+DIVYANSHNITJSR@users.noreply.github.com>
2024-01-13 11:26:24 +0530

    Time: 16 ms (68.96%) | Memory: 12.5 MB (32.52%) - LeetSync

--- BINARY/SEARCH/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ BINARY/SEARCH/583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int solve(int i,int j,string& s,string& s1,vector<vector<int>>& dp)
-{   if(i==0||j==0)
+{   if(i<0||j<0)
        return 0;
     if(dp[i][j]!=-1)
        return dp[i][j];
